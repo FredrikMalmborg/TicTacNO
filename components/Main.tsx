b83026927cb9b2813b970179d2d3fb27908966ff
Fred Malm <fredrik.malmborg@medieinstitutet.se>
2020-12-21 14:34:42 +0100

    main component

---
+++ components/Main.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import { Text, View } from 'react-native'
+
+
+const Main = () => {
+
+    return (
+        <View>
+            <Text>Hello Babiio!</Text>
+        </View>)
+}
+
+export default Main
