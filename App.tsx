262d4d2e9a907c229599210dc84e11b44c64a7df
ChristianAgren <christian.agren@medieinstitutet.se>
2020-12-21 15:59:22 +0100

    testing react native

--- App.tsx
+++ App.tsx
@@ -1,21 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  StyleProp,
+  StyleSheet,
+  Text,
+  TextStyle,
+  View,
+  ViewStyle,
+} from "react-native";
 
-export default function App() {
+interface IStyleManager {
+  container: StyleProp<ViewStyle>;
+  text: StyleProp<TextStyle>;
+}
+
+interface IProps {
+  dark: boolean;
+}
+
+export default function App({ ...props }) {
+  const dark = false;
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View style={styles({ ...props, dark }).container}>
+      <Text style={styles({ ...props, dark }).text}>
+        Open up App.tsx to start working on your game!
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create(
+  (props: IProps): IStyleManager => ({
+    container: {
+      flex: 1,
+      backgroundColor: `${props.dark ? "#000" : "#FFF"}`,
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    text: {
+      color: `${props.dark ? "#FFF" : "#000"}`,
+    },
+  })
+);
