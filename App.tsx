3b867445a116e3a2b058e7fd41acee99c857629d
ChristianAgren <christian.agren@medieinstitutet.se>
2020-12-27 21:31:11 +0100

    cleaned up layout structure

--- App.tsx
+++ App.tsx
@@ -1,46 +1,15 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import {
-  StyleProp,
-  StyleSheet,
-  Text,
-  TextStyle,
-  View,
-  ViewStyle,
-} from "react-native";
+import { View } from "react-native";
+import Background from "./components/background";
+import Pages from "./pages/pages";
 
-interface IStyleManager {
-  container: StyleProp<ViewStyle>;
-  text: StyleProp<TextStyle>;
-}
-
-interface IProps {
-  dark: boolean;
-}
-
-export default function App({ ...props }) {
-  const dark = false;
-
+export default function App() {
   return (
-    <View style={styles({ ...props, dark }).container}>
-      <Text style={styles({ ...props, dark }).text}>
-        Open up App.tsx to start working on your game!
-      </Text>
-      <StatusBar style="auto" />
+    <View>
+      <Pages />
+      <Background width="100%" height="100%" />
+      {/* <StatusBar style="light" /> */}
     </View>
   );
 }
-
-const styles = StyleSheet.create(
-  (props: IProps): IStyleManager => ({
-    container: {
-      flex: 1,
-      backgroundColor: `${props.dark ? "#000" : "#FFF"}`,
-      alignItems: "center",
-      justifyContent: "center",
-    },
-    text: {
-      color: `${props.dark ? "#FFF" : "#000"}`,
-    },
-  })
-);
---
+++ pages/startpage/startpage.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { View, StyleSheet, StyleProp, ViewStyle } from "react-native";
+import Logotype from "../../components/logotype";
+
+interface IStyles {
+    container: StyleProp<ViewStyle>
+}
+
+const StartPage = () => {
+  return (
+    <View style={style.container}>
+      <Logotype />
+    </View>
+  );
+};
+
+const style: IStyles = StyleSheet.create({
+    container: {
+        margin: "15%"
+    }
+})
+
+export default StartPage;
