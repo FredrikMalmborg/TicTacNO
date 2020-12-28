083c48782510ccb4230bcca70df48d0503e02b21
ChristianAgren <christian.agren@medieinstitutet.se>
2020-12-28 09:45:19 +0100

    small stuff

--- App.tsx
+++ App.tsx
@@ -1,15 +1,26 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { View } from "react-native";
+import { View, StyleSheet, StyleProp, ViewStyle } from "react-native";
 import Background from "./components/background";
 import Pages from "./pages/pages";
 
 export default function App() {
   return (
-    <View>
+    <View style={style.appContainer}>
       <Pages />
       <Background width="100%" height="100%" />
-      {/* <StatusBar style="light" /> */}
+      <StatusBar style="dark" />
     </View>
   );
 }
+
+const style: { appContainer: StyleProp<ViewStyle> } = StyleSheet.create({
+  appContainer: {
+    position: "absolute",
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
+    zIndex: 1
+  },
+});
