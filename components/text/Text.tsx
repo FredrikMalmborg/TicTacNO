bf8a3b5e15f3adbc1277fd5a20cc0c1ba9f4efcd
Lint Action <lint-action@samuelmeuli.com>
2020-12-29 15:06:35 +0000

    Fix code style issues with Prettier

--- components/text/Text.tsx
+++ components/text/Text.tsx
@@ -1,95 +1,115 @@
-import React from 'react'
-import { View, Text, StyleProp, ViewStyle, TextStyle, StyleSheet, ImageStyle } from 'react-native'
-import { Svg, Path, Image } from 'react-native-svg'
+import React from "react";
+import {
+  View,
+  Text,
+  StyleProp,
+  ViewStyle,
+  TextStyle,
+  StyleSheet,
+  ImageStyle,
+} from "react-native";
+import { Svg, Path, Image } from "react-native-svg";
 
-import Play_PNG  from "../../assets/images/text/Play_PNG.png";
-import Profile_PNG  from "../../assets/images/text/Profile_PNG.png";
-import Host_PNG  from "../../assets/images/text/Host_PNG.png";  
-import Join_PNG  from "../../assets/images/text/Join_PNG.png";
-
-
+import Play_PNG from "../../assets/images/text/Play_PNG.png";
+import Profile_PNG from "../../assets/images/text/Profile_PNG.png";
+import Host_PNG from "../../assets/images/text/Host_PNG.png";
+import Join_PNG from "../../assets/images/text/Join_PNG.png";
 
 import {
-    useFonts,
-    FredokaOne_400Regular
-  } from '@expo-google-fonts/fredoka-one'	
+  useFonts,
+  FredokaOne_400Regular,
+} from "@expo-google-fonts/fredoka-one";
 
-type Size = "sm" | "md" | "lg" | number
+type Size = "sm" | "md" | "lg" | number;
 
 interface IStyles {
-    container: StyleProp<ViewStyle>;
-    text: StyleProp<TextStyle>;
-  }
+  container: StyleProp<ViewStyle>;
+  text: StyleProp<TextStyle>;
+}
 
 interface Props {
-    children?: React.ReactNode
-    label?: string
-    title?: boolean  
-    bread?: boolean  
-    size?: Size 
+  children?: React.ReactNode;
+  label?: string;
+  title?: boolean;
+  bread?: boolean;
+  size?: Size;
 }
 
 const TicTacText = (props: Props) => {
-    const [fontsLoaded /*, error */] = useFonts({FredokaOne_400Regular});
-    const [content, setContent] = React.useState(<></>)
-    
-    const {size, title, label, bread, children} = props
-    
-    const getSize = () => {
-        switch(size) {
-            case 'sm': return 25 
-            case 'md': return 50 
-            case 'lg': return 100 
-            default: return size
-        }
+  const [fontsLoaded /*, error */] = useFonts({ FredokaOne_400Regular });
+  const [content, setContent] = React.useState(<></>);
+
+  const { size, title, label, bread, children } = props;
+
+  const getSize = () => {
+    switch (size) {
+      case "sm":
+        return 25;
+      case "md":
+        return 50;
+      case "lg":
+        return 100;
+      default:
+        return size;
     }
-    // const imgScale: string = getScale();
-    const getImg = () => {
-        let img
-        switch(label?.toLocaleLowerCase()) {
-            case 'play' : img = Play_PNG; break;
-            case 'profile' : img = Profile_PNG; break;
-            case 'join' : img = Join_PNG; break;
-            case 'host' : img = Host_PNG; break;
-        }
-        
-        return (
-        <Svg style={style().container} height={getSize()}>
-            <Image width="100%" height={"100%"} href={img}/>
-        </Svg>)
-    }
-    const getText = () => {
-        return (
-        <View style={style({bread}).container}>
-            <Text style={style({bread}, getSize()).text}>{ children ? children : label}</Text>
-        </View>)
+  };
+  // const imgScale: string = getScale();
+  const getImg = () => {
+    let img;
+    switch (label?.toLocaleLowerCase()) {
+      case "play":
+        img = Play_PNG;
+        break;
+      case "profile":
+        img = Profile_PNG;
+        break;
+      case "join":
+        img = Join_PNG;
+        break;
+      case "host":
+        img = Host_PNG;
+        break;
     }
 
-    
-    React.useEffect(() => {
-        setContent(title ? getImg() : getText())
-    }, [])
+    return (
+      <Svg style={style().container} height={getSize()}>
+        <Image width="100%" height={"100%"} href={img} />
+      </Svg>
+    );
+  };
+  const getText = () => {
+    return (
+      <View style={style({ bread }).container}>
+        <Text style={style({ bread }, getSize()).text}>
+          {children ? children : label}
+        </Text>
+      </View>
+    );
+  };
 
-    
-    return <>{content}</>
-}
+  React.useEffect(() => {
+    setContent(title ? getImg() : getText());
+  }, []);
 
-const style = (type?: {bread?: boolean, title?: boolean}, size?: number): IStyles => {
-    
-    return(
-        StyleSheet.create({
-            container: {
-              width:"100%",
+  return <>{content}</>;
+};
 
-              border: '1px solid red',
-              margin: type?.bread ? "" : 10
-            },
-            text: {
-                fontFamily: type?.bread ? 'sans-serif' : 'FredokaOne_400Regular' ,
-                fontSize: size || 20
-            }
-          })
-    )
-}
+const style = (
+  type?: { bread?: boolean; title?: boolean },
+  size?: number
+): IStyles => {
+  return StyleSheet.create({
+    container: {
+      width: "100%",
 
-export default TicTacText
+      border: "1px solid red",
+      margin: type?.bread ? "" : 10,
+    },
+    text: {
+      fontFamily: type?.bread ? "sans-serif" : "FredokaOne_400Regular",
+      fontSize: size || 20,
+    },
+  });
+};
+
+export default TicTacText;
--- pages/gamepage_test/GamePage.tsx
+++ pages/gamepage_test/GamePage.tsx
@@ -1,12 +1,18 @@
 import React from "react";
-import { View, StyleSheet, StyleProp, ViewStyle, Dimensions } from "react-native";
+import {
+  View,
+  StyleSheet,
+  StyleProp,
+  ViewStyle,
+  Dimensions,
+} from "react-native";
 import { Grid, Row, Col } from "react-native-easy-grid";
 import Background from "../../components/background";
 
 import {
-    useFonts,
-    FredokaOne_400Regular
-  } from '@expo-google-fonts/fredoka-one'	
+  useFonts,
+  FredokaOne_400Regular,
+} from "@expo-google-fonts/fredoka-one";
 
 interface IStyles {
   container: StyleProp<ViewStyle>;
@@ -17,46 +23,42 @@ interface IStyles {
   o: StyleProp<ViewStyle>;
 }
 
-
-
 const GamePage = () => {
+  const [fontsLoaded /*, error */] = useFonts({ FredokaOne_400Regular });
 
-    const [fontsLoaded /*, error */] = useFonts({FredokaOne_400Regular});
+  const board = [
+    [1, 0, 0, 2, 0],
+    [2, 1, 1, 1, 0],
+    [0, 1, 2, 3, 3],
+    [1, 1, 2, 1, 0],
+    [0, 3, 0, 2, 3],
+  ];
 
-    const board = [
-        [1,0,0,2,0],
-        [2,1,1,1,0],
-        [0,1,2,3,3],
-        [1,1,2,1,0],
-        [0,3,0,2,3]
-    ]
-    
-const windowWidth = Dimensions.get('window').width;
-const windowHeight = Dimensions.get('window').height;
+  const windowWidth = Dimensions.get("window").width;
+  const windowHeight = Dimensions.get("window").height;
 
-const length = windowWidth > windowHeight ? windowHeight : windowWidth
+  const length = windowWidth > windowHeight ? windowHeight : windowWidth;
 
   return (
-    <View style={[style.container,{width: length , height: length}]}>
-        <Grid style={{width: '100%', height:'100%'}}>
-        {
-            board.map((row) => (
-                <Row>
-                    {
-                        row.map((col) => {
-                            switch(col) {
-                                case 0: return <Col style={[style.cell,style.deactivated]}></Col>
-                                case 1: return <Col style={[style.cell,style.empty]}></Col>
-                                case 2: return <Col style={[style.cell,style.x]}>X</Col>
-                                case 3: return <Col style={[style.cell, style.o]}>O</Col>
-                            }
-                        }
-                        )
-                    }      
-                </Row>
-            ))
-        }
-        </Grid>
+    <View style={[style.container, { width: length, height: length }]}>
+      <Grid style={{ width: "100%", height: "100%" }}>
+        {board.map((row) => (
+          <Row>
+            {row.map((col) => {
+              switch (col) {
+                case 0:
+                  return <Col style={[style.cell, style.deactivated]}></Col>;
+                case 1:
+                  return <Col style={[style.cell, style.empty]}></Col>;
+                case 2:
+                  return <Col style={[style.cell, style.x]}>X</Col>;
+                case 3:
+                  return <Col style={[style.cell, style.o]}>O</Col>;
+              }
+            })}
+          </Row>
+        ))}
+      </Grid>
     </View>
   );
 };
@@ -64,41 +66,41 @@ const length = windowWidth > windowHeight ? windowHeight : windowWidth
 const style: IStyles = StyleSheet.create({
   container: {
     // flex: 1,
-    justifyContent:"center",
+    justifyContent: "center",
     alignItems: "center",
-    
+
     padding: 30,
-},
-cell: {
-      justifyContent:"center",
-      alignItems: "center",
-      fontFamily: 'FredokaOne_400Regular',
-      color: '#fff',
-      fontSize:80,
+  },
+  cell: {
+    justifyContent: "center",
+    alignItems: "center",
+    fontFamily: "FredokaOne_400Regular",
+    color: "#fff",
+    fontSize: 80,
 
-      margin: 10,
-      borderRadius: 10,
+    margin: 10,
+    borderRadius: 10,
 
-      border: '3px solid'
-},
+    border: "3px solid",
+  },
 
-deactivated: {
-    opacity: 0.33
-},
-empty: {
+  deactivated: {
+    opacity: 0.33,
+  },
+  empty: {
     borderColor: "#D7D7D7",
     backgroundColor: "#fff",
-    boxShadow: '-4px 4px 0px #D7D7D7'
-},
-x: {
-    borderColor:'#BB2832',
+    boxShadow: "-4px 4px 0px #D7D7D7",
+  },
+  x: {
+    borderColor: "#BB2832",
     backgroundColor: "#fa5457",
-    boxShadow: '-4px 4px 0px #BB2832'
-},
-o: {
-    borderColor:'#0B797D',
+    boxShadow: "-4px 4px 0px #BB2832",
+  },
+  o: {
+    borderColor: "#0B797D",
     backgroundColor: "#03D2DB",
-    boxShadow: '-4px 4px 0px #0B797D'
+    boxShadow: "-4px 4px 0px #0B797D",
   },
 });
 
