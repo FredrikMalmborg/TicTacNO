e5ff8092c9d7bb2b5a1348bfe1d08c0948061329
ChristianAgren <christian.agren@medieinstitutet.se>
2021-01-11 11:00:21 +0100

    added types and deps

---
+++ types.d.ts
@@ -0,0 +1,13 @@
+declare module "@env" {
+  export const API_KEY: string;
+  export const AUTH_DOMAIN: string;
+  export const DATABASE_URL: string;
+  export const PROJECT_ID: string;
+  export const STORA_BUCKET: string;
+  export const MESSAGE_SENDER_ID: string;
+  export const APP_ID: string;
+  export const MEASUREMENT_ID: string;
+  export const ANDROID_CLIENT_ID: string;
+  export const IOS_CLIENT_ID: string;
+  export const FACEBOOK_APP_ID: string;
+}
